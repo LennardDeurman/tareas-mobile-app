5b3235740f1b5651c3ab7f42edcc2dc4c0d3ae7f
Lennard <lennarddeurman@live.nl>
2020-06-13 14:57:06 +0200

    Configured authorization via universal long

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter_i18n/flutter_i18n_delegate.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:tareas/extensions/asset_paths.dart';
 import 'package:tareas/extensions/custom_fonts.dart';
-import 'package:tareas/pages/login.dart';
 
 Future main() async {
   final FlutterI18nDelegate flutterI18nDelegate = FlutterI18nDelegate(
@@ -31,7 +30,7 @@ class MainApp extends StatelessWidget {
         fontFamily: CustomFonts.openSans
       ),
       home: LoginPage(),
-      localizationsDelegates: [
+      localizationsDelegates: [  
         flutterI18nDelegate,
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate
