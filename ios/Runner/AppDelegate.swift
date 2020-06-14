074c778bebf0bed633366f85755b2800768a0d75
deurman <lennarddeurman@live.nl>
2020-06-14 02:08:00 +0200

    Added AuthResult class

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import flutter_auth0
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,5 +10,9 @@ import Flutter
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+    return FlutterAuth0Plugin.application(app, open: url, options: options)
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_auth0/flutter_auth0.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_i18n/flutter_i18n_delegate.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -26,12 +27,99 @@ class HomePage extends StatelessWidget {
 
 }
 
+class AuthResult {
+
+  String idToken;
+  String accessToken;
+  String refreshToken;
+  DateTime expiryDate;
+
+
+  AuthResult(Map<String, dynamic> map) {
+    idToken = map["idToken"];
+    accessToken = map["accessToken"];
+    refreshToken = map["refreshToken"];
+    if (map.containsKey("expiryDate")) {
+      expiryDate = DateTime.fromMillisecondsSinceEpoch(
+          map["expiryDate"]
+      );
+    } else if (map.containsKey("expiresIn")) {
+      expiryDate = DateTime.fromMillisecondsSinceEpoch(
+          DateTime.now().millisecondsSinceEpoch + (map["expiresIn"] * 1000)
+      );
+    }
+  }
+
+  Future save() {
+    
+  }
+
+  static Future<AuthResult> loadCurrent() {
+
+  }
+
+}
+
+
+class AuthService {
+
+  /*
+
+  To setup auth0 app:
+
+  1. Make sure the Android manifest contains the necessary setting
+  2. Make sure info.plist for iOS is changed according to documentation
+  3. Add the callbacks urls in the dashboard as valid:
+
+  com.tareas.app://dev-vpji-vk5.eu.auth0.com/android/com.tareas.app/callback
+  com.tareas.app://dev-vpji-vk5.eu.auth0.com/ios/com.tareas.app/callback
+
+  Change dev-vpji-vk5.eu.auth0.com to the correct domain
+
+  4. Change the clientId and domain in this class
+
+
+  In order to change the application login client the following steps are needed:
+
+  1. Change the domain in the Android manifest (for iOS no changes needed)
+  2. Change the clientId and domain in the Auth class
+
+   */
+
+  static const String clientId = "Ntdv2VVHrHRGkNvPhGi4l6fd8rF7Eu40";
+  static const String domain = "dev-vpji-vk5.eu.auth0.com";
+  static const String baseUrl = "https://$domain/";
+
+  final Auth0 auth0 = Auth0(baseUrl: baseUrl, clientId: clientId);
+
+  Future authorize() async {
+    return await
+    auth0.
+    webAuth.
+    authorize({
+      'audience': 'https://$domain/userinfo',
+      'scope': 'openid email offline_access',
+    });
+  }
+
+
+  void initialize() async {
+
+    var response = await authorize();
+
+  }
+
+
+}
 
 class MainApp extends StatelessWidget {
 
   final FlutterI18nDelegate flutterI18nDelegate;
+  final AuthService authService = AuthService();
 
-  MainApp (this.flutterI18nDelegate);
+  MainApp (this.flutterI18nDelegate) {
+    authService.initialize();
+  }
 
   @override
   Widget build(BuildContext context) {
